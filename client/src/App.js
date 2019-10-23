d82bc17c1f53aa640252e6830bba785f7d8b032a
William Post <wpost@hawaii.edu>
2019-10-23 11:47:04 -1000

    added basic client code with create-react-app, added basic page that calls our server api

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+const API_BASE = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api';
+
+function App() {
+  const [message, setMessage] = useState("Enter your name below:");
+  const [name, setName] = useState("");
+
+    async function onClick() {
+      try {
+          const response = await fetch(`${API_BASE}/hello`, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({name})
+          });
+          const respJson = await response.json();
+          setMessage(respJson.message);
+      } catch (e) {
+          setMessage("Sorry there was an error with your request. Please try again:")
+      }
+  }
+
+  return (
+      <>
+        <h1>{message}</h1>
+        <input type={"text"} onChange={(e) => setName(e.target.value)}/>
+          <button onClick={onClick}>Submit</button>
+      </>
+  );
+
+}
+
+export default App;
