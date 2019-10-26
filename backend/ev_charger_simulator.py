3d29ba462654969123d3c974f5e15008a9de544e
Paul Spooren <mail@aparcar.org>
2019-10-25 15:34:02 -1000

    backend: extend simulator with position
    also last use to send alerts if inactive for too long
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- backend/ev_charger_simulator.py
+++ backend/ev_charger_simulator.py
@@ -6,31 +6,48 @@ import sys
 import datetime as dt
 from os import environ
 
-polls = []
+# these environment variables are setable in the docker-compose.yml
 
-
+# station name, a corresponding json file must exists
 station = environ.get("STATION", "A")
+
+# what port to listen on
 port = environ.get("PORT", 8000)
 
+# position
+latitude = environ.get("LAT", 21.300672)
+longitude = environ.get("LONG", -157.851773)
+
+
+polls = []
 with open(f"polls_{station}.json") as polls_file:
     polls = json.load(polls_file)
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
+metrics_prefix = "evc_"  # electic vehicle charger
 
-info = Info("ev_charger", "EV Charger name and position")
-info.info({"charger_name": station})
+# general information about the charger
+info = Info(f"{metrics_prefix}info", "EV Charger name meta information")
+
 payment_mode = Enum(
-    "payment_method", "Payment method currently used", states=["", "RFID", "CREDITCARD"]
+    f"{metrics_prefix}payment_method",
+    "Payment method currently used",
+    states=["", "RFID", "CREDITCARD"],
 )
 port_type = Enum(
-    "port_type", "Port type currently used", states=["", "CHADEMO", "DCCOMBOTYP1"]
+    f"{metrics_prefix}port_type",
+    "Port type currently used",
+    states=["", "CHADEMO", "DCCOMBOTYP1"],
 )
 session_initiated_by = Enum(
-    "session_initiated_by", "Session initiated by", states=["", "MOBILE", "DEVICE"]
+    f"{metrics_prefix}evc_session_initiated_by",
+    "Session initiated by",
+    states=["", "MOBILE", "DEVICE", "WEB"],
 )
 
-kwh = Gauge("kwh", "Description of gauge")
+kwh = Gauge(f"{metrics_prefix}kwh", "Current kwh usage")
+last_charge_seconds = Gauge(
+    f"{metrics_prefix}last_charge", "UNIX timestamp of last use"
+)
 
 
 # Decorate function with metric.
@@ -49,17 +66,28 @@ def process_request():
         i += 1
 
     for poll in polls[i:]:
-        info.info({"session_id": poll[2]})
+        info.info(
+            {
+                "charger_name": f"Station {station}",
+                "latitude": str(latitude),
+                "longitude": str(longitude),
+                "session_id": poll[2],
+            }
+        )
         kwh.set(poll[5])
+        # update the last_charge value if a charge is detected
+        if poll[5] > 0:
+            last_charge_seconds.set_to_current_time()
+
         session_initiated_by.state(poll[1])
         port_type.state(poll[3])
         payment_mode.state(poll[4])
-        print(poll)
-        sys.stderr.write(str(poll))
+        sys.stderr.write(str(poll) + "\n")
         time.sleep(5 * 60)
 
 
 if __name__ == "__main__":
+    sys.stderr.write("Starting EV Simulator\n")
     start_http_server(port)
     while True:
         process_request()
