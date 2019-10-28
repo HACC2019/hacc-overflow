51aa20d4afd4c951bef17eaa7964c1ca53ad2162
William Post <wpost@hawaii.edu>
2019-10-27 19:46:22 -1000

    refactored so except simple get args instead of json

--- flask_api/avg_duration/get_avg.py
+++ flask_api/avg_duration/get_avg.py
@@ -4,8 +4,8 @@ from flask import request, jsonify, Blueprint
 STATION_A = "station_a"
 STATION_B = "station_b"
 
-STATION_A_F_NAME = "flask_api/get_average/station_A_Averages.json"
-STATION_B_F_NAME = "flask_api/get_average/station_B_Averages.json"
+STATION_A_F_NAME = "flask_api/avg_duration/station_A_Averages.json"
+STATION_B_F_NAME = "flask_api/avg_duration/station_B_Averages.json"
 
 stations_to_avg_files = [(STATION_A, STATION_A_F_NAME), (STATION_B, STATION_B_F_NAME)]
 
@@ -19,36 +19,30 @@ for station_name, f_name in stations_to_avg_files:
         data = json.load(json_file)
         stations_to_avgs[station_name] = data
 
-getavg_api = Blueprint('getavg_api', __name__)
+avg_duration_api = Blueprint('getavg_api', __name__)
 
-@getavg_api.route('/getavg', methods=["GET", "POST"])
-def average():
-    station = None
-    hour = None
-    try:
-        station = request.json["station"]
-        if station not in stations_to_avgs:
-            return jsonify({"err": "Station not found."}), 400
 
-    except TypeError:
-        return jsonify({"error": "No post json data was given."}), 400
-    except KeyError:
-        return jsonify({"error": "No station name was given."}), 400
+@avg_duration_api.route('/avg-duration', methods=["GET"])
+def get_avg():
+    """
+    Returns average duration of a charge for a given hour and station.
+    Args (GET):
+        station (string): station name in form 'station_X' where X is the station id.
+        hour (int): hour (0-23)
+    Returns (JSON):
+        average (200): found average for given hour
+        error (400): if params are missing or an average was not be found.
+    """
 
-    try:
-        hour = str(request.json["hour"])
-    except KeyError:
-        return jsonify({"error": "No hour was given."}), 400
-    except ValueError:
-        return jsonify({"error": "hour was not able to be casted to int"}), 400
+    station = request.args.get("station")
+    hour = request.args.get("hour")
+
+    if not station or not hour:
+        return jsonify({"error": "No station or hour was given."}), 400
 
     try:
-        print(station)
-        print(hour)
         avg = stations_to_avgs[station][hour]
     except KeyError:
         return jsonify({"err": "Unable to find average with given station and hour"}), 400
 
     return jsonify({"average": avg}), 200
-
-
