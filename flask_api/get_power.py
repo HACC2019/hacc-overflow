c003111108f94ece4acc71d4d03f07b54af2b90c
William Post <wpost@hawaii.edu>
2019-11-01 09:56:15 -1000

    edited power_graph to be more accurate

--- flask_api/get_power.py
+++ flask_api/get_power.py
@@ -3,6 +3,32 @@ import numpy as np
 
 get_power_blueprint = Blueprint('get_power_blueprint', __name__)
 
+# a, b, c are constants used in power function
+b = np.float64("0.0786688810066")
+
+
+def get_power_func(energy, duration):
+    # given energy, and duration, generates power function.
+    # energy must be in kwM and duration must be in minutes, since function maps
+    # given HECO graph.
+    e = energy
+    t = duration
+
+    a_numerator = np.square(b) * e * (b * np.sqrt(t) + 1)
+    a_denominator = np.square(b) * t
+    a_denominator = a_denominator + 2 * b * np.sqrt(t)
+    a_denominator = a_denominator - 2 * b * np.sqrt(t) * np.log(b * np.sqrt(t) + 1)
+    a_denominator = a_denominator - 2 * np.log(b * np.sqrt(t) + 1)
+    a = np.divide(a_numerator, a_denominator)
+    c = np.divide(-a, 1 + b*np.sqrt(t))
+
+    def f(x):
+        if x >= t:
+            return 0.0  # function should always go to 0 after time t
+        return c + np.divide(a, 1 + b*np.sqrt(x))
+
+    return f
+
 
 @get_power_blueprint.route('/get_power', methods=["GET"])
 def get_power():
@@ -26,23 +52,15 @@ def get_power():
     try:
         duration = np.double(duration)
         duration = np.divide(duration, 60)  # found function is in minutes
-        energy = np.double(duration)
+        energy = np.double(energy)
+        energy = energy * 60
         intervals = int(intervals)
     except ValueError:
         return jsonify({"error": "given was unable to be casted to numbers."}), 400
 
-    if duration <= .5:
-        return jsonify(({"error": "duration too short, function is undefined"}))
-
-    c = np.divide(1, (16 * duration - 8))
-    k = -4 * energy * np.sqrt(c) * np.sqrt(duration + c)
-    k = np.divide(k, np.sqrt(c) - np.sqrt(duration + c))
-    j = np.divide(-k, np.sqrt(duration + c))
-
-    f_of_x = lambda x: np.divide(k, np.sqrt(x + c)) + j
-
+    f = get_power_func(energy, duration)
     interval_len = np.divide(duration, intervals)
 
-    power_by_interval = [f_of_x(i * interval_len) for i in range(intervals)]
+    power_by_interval = [f(i * interval_len) for i in range(intervals)]
 
     return jsonify({"power_by_interval": power_by_interval}), 200
