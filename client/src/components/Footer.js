1203b244b13c47d6418970d1e0a58d0d4c552cd3
Jon Bach <bach4@hawaii.edu>
2019-11-11 15:18:44 -1000

    .

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,7 +17,20 @@ export default function Footer(){
       <footer className={classes.footer} >
         <Typography variant="h6" align="center" gutterBottom>
           Temporary Simple Footer
+
+        <div>
+          <a href={"https://hacc.hawaii.gov/"}>
+            Hawai'i Annual Code Challenge 2019.
+          </a>
+        </div>
+        <div>
+          <a href={"https://www.hawaiianelectric.com/products-and-services/electric-vehicles"}>
+            HECO Electric Vehicles
+          </a>
+        </div>
+
         </Typography>
+
       </footer>
   );
 }
